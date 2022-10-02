5a1dbfc350b1e4273e20838495a0b8963aeaedc6
yyj <99321359+youngjae-avikus@users.noreply.github.com>
2022-10-02 13:25:40 +0900

    Create interact_show_sample.py

---
+++ interact_show_sample.py
@@ -0,0 +1,27 @@
+@interact(index=(0, len(image_files)-1))
+def show_sample(index=0):
+    image_file = image_files[index]
+    image_path = os.path.join("../DATASET/Detection/images/", image_file)
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    image_id = image_file.split('.')[0]
+    meta_data = data_df[data_df['ImageID'] == image_id]
+    cate_names = meta_data["LabelName"].values
+    bboxes = meta_data[["XMin", "XMax", "YMin", "YMax"]].values
+    
+    img_H, img_W, _ = image.shape
+
+    class_ids = [CLASS_NAME_TO_ID[cate_name] for cate_name in cate_names]
+    unnorm_bboxes = bboxes.copy()
+    unnorm_bboxes[:, [1,2]] = unnorm_bboxes[:, [2,1]]
+    unnorm_bboxes[:, 2:4] -= unnorm_bboxes[:, 0:2]
+    unnorm_bboxes[:, 0:2] += (unnorm_bboxes[:, 2:4] / 2)
+    unnorm_bboxes[:, [0,2]] *= img_W
+    unnorm_bboxes[:, [1,3]] *= img_H
+    canvas = visualize(image, unnorm_bboxes, class_ids)
+    
+    plt.figure(figsize=(6,6))
+    plt.imshow(canvas)
+    plt.axis('off')
+    plt.show()
