8df3dd73cdecf12ae4da94fb92d9dc0da7e61fab
yyj <99321359+youngjae-avikus@users.noreply.github.com>
2022-10-15 15:19:01 +0900

    Update visualize_yolo.py

--- visualize_yolo.py
+++ visualize_yolo.py
@@ -1,39 +1,86 @@
 import os
 import cv2
-import torch
 
-CLASS_NAME_TO_ID = {'Bus': 0, 'Truck': 1}
-CLASS_ID_TO_NAME = {0: 'Bus', 1: 'Truck'}
-BOX_COLOR = {'Bus':(200, 0, 0), 'Truck':(0, 0, 200)}
-TEXT_COLOR = (255, 255, 255)
+CLASS_NAME_TO_ID = {'Buoy': 0, 'Boat': 1, 'Channel Marker': 2, 'Speed Warning Sign': 3}
+CLASS_ID_TO_NAME = {0: 'Buoy', 1: 'Boat', 2: 'Channel Marker', 3: 'Speed Warning Sign'}
+SIZE=(1920,1080)
 
-
-def visualize_bbox(image, bbox, class_name, color=BOX_COLOR, thickness=2):
-    x_center, y_center, w, h = bbox
-    x_min = int(x_center - w/2)
-    y_min = int(y_center - h/2)
-    x_max = int(x_center + w/2)
-    y_max = int(y_center + h/2)
+def visualize_yolo(image, bboxes, category_ids, color=None):
+    image = copy.deepcopy(image)
     
-    cv2.rectangle(image, (x_min, y_min), (x_max, y_max), color=color[class_name], thickness=thickness)
-    
-    ((text_width, text_height), _) = cv2.getTextSize(class_name, cv2.FONT_HERSHEY_SIMPLEX, 0.35, 1)    
-    cv2.rectangle(image, (x_min, y_min - int(1.3 * text_height)), (x_min + text_width, y_min), color[class_name], -1)
-    cv2.putText(
-        image,
-        text=class_name,
-        org=(x_min, y_min - int(0.3 * text_height)),
-        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-        fontScale=0.35, 
-        color=TEXT_COLOR, 
-        lineType=cv2.LINE_AA,
-    )
+    if color is None:
+        colors = Colors()
+    else:
+        colors = None
+        
+    for bbox, category_id in zip(bboxes, category_ids):
+        class_name = CLASS_ID_TO_NAME[category_id[0]]
+        
+        x_center, y_center, w, h = bbox
+        x_min = int((x_center - w/2) * SIZE[0])
+        y_min = int((y_center - h/2) * SIZE[1])
+        x_max = int((x_center + w/2) * SIZE[0])
+        y_max = int((y_center + h/2) * SIZE[1])
+
+        if colors is not None:
+            color = colors(category_id[0])
+        
+        # set rect_th for boxes
+        rect_th = max(round(sum(image.shape) / 2 * 0.001), 1)
+        # set text_th for category names
+        text_th = max(rect_th - 1, 1)
+        # set text_size for category names
+        text_size = rect_th / 3
+
+        cv2.rectangle(image, 
+                      (x_min, y_min),
+                      (x_max, y_max), 
+                      color=color,
+                      thickness=rect_th)
+        
+        p1, p2 = (int(x_min), int(y_min)), (int(x_max), int(y_max))
+        label = f"{class_name}"
+        w, h = cv2.getTextSize(label, 0, fontScale=text_size, thickness=text_th)[0]  # label width, height
+        outside = p1[1] - h - 3 >= 0  # label fits outside box
+        p2 = p1[0] + w, p1[1] - h - 3 if outside else p1[1] + h + 3
+        
+        cv2.rectangle(image, p1, p2, color, -1, cv2.LINE_AA)  # filled
+        cv2.putText(
+            image,
+            label,
+            (p1[0], p1[1] - 2 if outside else p1[1] + h + 2),
+            0,
+            text_size,
+            (255, 255, 255),
+            thickness=text_th,
+        )
+        
     return image
 
+label_files = sorted(glob(yolo_label_dir+"/*.txt"))
+image_files = sorted(glob(yolo_image_dir+"/*.jpg"))
 
-def visualize(image, bboxes, category_ids):
-    img = image.copy()
-    for bbox, category_id in zip(bboxes, category_ids):
-        class_name = CLASS_ID_TO_NAME[category_id]
-        img = visualize_bbox(img, bbox, class_name)
-    return img
+@interact(index=(0,len(image_files)))
+def verify_gt(index=0):
+    image = cv2.imread(image_files[index])
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    label = label_files[index]
+    
+    bboxes = []
+    category_ids = []
+    
+    with open(label, 'r') as f:
+        
+        line = f.readline().strip()
+        while line:
+            id, cx, cy, w, h = list(map(float, line.split()))
+            bboxes.append([cx, cy, w, h])
+            category_ids.append([int(id)])
+            line = f.readline().strip()
+
+    if len(bboxes) > 0:    
+        canvas = visualize_yolo(image, bboxes, category_ids)
+        plt.figure(figsize=(16,16))
+        plt.imshow(canvas)
+        plt.axis('off')
+        plt.show()
